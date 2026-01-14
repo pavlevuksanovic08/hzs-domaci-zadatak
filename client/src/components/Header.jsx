6c54a591ed4ca1ba129bc906f5ec633a83300375
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-01-14 16:00:00 +0100

    create header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <h1>Healtify</h1>
+            </div>
+        </header>
+        
+    )
+}
