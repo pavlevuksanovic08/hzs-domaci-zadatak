e3bc8fcde49af825a2f7bd2151c34d0be18ad330
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-01-14 02:06:48 +0100

    make app structure and style layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
 import './App.css'
+import "./styles/layout.css"
 
 function App() {
 
+  const [mainPage, setMainPage] = useState("goals")
+
   return (
     <>
-      
+      <Header />
+      <Menu />
+      <Main />
     </>
   )
 }
