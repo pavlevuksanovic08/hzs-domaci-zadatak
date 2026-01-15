8e44ce2f9c41e70c1b9522a054dcad1c148ed8db
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-01-15 01:31:51 +0100

    style header and display proper text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,18 @@
 
-export default function Header() {
+import "../styles/header.css";
+
+export default function Header({ state }) {
     return (
         <header>
             <div>
-                <h1>Healtify</h1>
+                {state === "goals" ?        
+                    <h1>Goals Dashboard</h1>
+                    : state === "statistics" ?
+                        <h1>Statistics Overview</h1>
+                    :   <h1>User Profile</h1>
+            }
             </div>
+    
         </header>
         
     )
